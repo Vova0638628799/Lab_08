2435257a12a9c6b654273fb46378964ab2bcaa7e
Vova0638628799 <120423336+Vova0638628799@users.noreply.github.com>
2022-12-13 00:50:05 +0200

    Add files via upload

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main_0',['main',['../main_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.c']]],
+  ['main_2ec_1',['main.c',['../main_8c.html',1,'']]],
+  ['my_20project_2',['My Project',['../index.html',1,'']]]
+];
